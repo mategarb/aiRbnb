a12bd3dd7157d9960c8768ee26720468d954606b
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2019-06-16 22:39:57 +0200

    Interactive map

--- R/map_example.R
+++ R/map_example.R
@@ -3,8 +3,37 @@ library(geojsonR)
 
 nycounties <- geojson_read("/Users/karsm933/Downloads/neighbourhoods.geojson",
                            what = "sp")
-d = palette(rainbow(14))
+data <- read.csv('/Users/karsm933/Downloads/listings.csv')
+
+
+x = sample(1:7, 14,replace = TRUE)
+bins <- seq(1:7)
+pal <- colorBin("YlOrRd", domain = x, bins = bins)
+labels <- sprintf(
+  "<strong>%s</strong><br/> rate: %g<sup></sup>",
+  as.character(nycounties$neighbourhood), x
+) %>% lapply(htmltools::HTML)
+
 
 leaflet(nycounties) %>%
   addTiles() %>% addProviderTiles(providers$Stamen.TonerLite) %>%
-  addPolygons(fillColor = d, stroke = FALSE)
+  addPolygons(
+  fillColor = ~pal(x),
+weight = 2,
+opacity = 1,
+color = "white",
+dashArray = "3",
+fillOpacity = 0.7,
+highlight = highlightOptions(
+  weight = 5,
+  color = "#666",
+  dashArray = "",
+  fillOpacity = 0.7,
+  bringToFront = TRUE),
+label = labels,
+labelOptions = labelOptions(
+  style = list("font-weight" = "normal", padding = "3px 8px"),
+  textsize = "15px",
+  direction = "auto")) %>%
+  addLegend(pal = pal, values = ~x, opacity = 0.7, title = NULL,
+            position = "bottomright")
