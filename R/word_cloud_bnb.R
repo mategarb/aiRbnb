124fc01a91cc68d64418fd3b5c02b4d5a3011268
mategarb <mateuszgarbulowski@gmail.com>
2019-06-19 11:24:04 +0200

    error fixed

--- R/word_cloud_bnb.R
+++ R/word_cloud_bnb.R
@@ -72,6 +72,7 @@ if(any(str_detect(info_2, info))) {
 
 colfunc <- colorRampPalette(c("darkorchid1","firebrick1", "chartreuse", "deepskyblue", "gold"))
 cols <- colfunc(length(words_inp$freq))
+words_inp <- words_inp[,-2]
 
 colnames(words_inp) <- c("word", "freq")
 wordcloud2(words_inp, color=cols)
