e5abba2db5063fe4a4a9f88e1e5b8877d0733ba7
mategarb <mateuszgarbulowski@gmail.com>
2019-06-19 17:50:38 +0200

    changes provided

--- R/remove_outs.R
+++ R/remove_outs.R
@@ -1,5 +1,12 @@
 remove_outs <- function(x, q_min="10%", q_max="90%") {
 
+  if(!is.character(q_min) | !is.character(q_max)) {
+    w <- simpleWarning("Quantile thresholds should be character. Qmin and Qmax set to default")
+    w %>% print
+    q_min <- "10%"
+    q_max <- "90%"
+  }
+
   qnts <- quantile(x, probs=seq(0,1,by=.05))
   cut_1 <- qnts %>% enframe %>% filter(name==q_min) %>% .$value
   cut_2 <- qnts %>% enframe %>% filter(name==q_max) %>% .$value
